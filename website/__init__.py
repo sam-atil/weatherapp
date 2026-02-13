e2d64fee5bf5a064d7c2c0fed27c79072cb66894
Samuel Atilano <samuel4atilano@icloud.com>
2026-02-13 13:24:22 -0500

    Updating requirements.txt file and added .python-version file

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,15 @@
-from flask import Flask
-# from models import db, User
-from flask_login import LoginManager
 # from views import main_blueprint
 # from auth import auth_blueprint
 import os
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv #Works for local server, needs .env file to work
+
+from dotenv import \
+    load_dotenv  # Works for local server, needs .env file to work
+from flask import Flask
+# from models import db, User
+from flask_login import LoginManager
+
 load_dotenv()
 
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
@@ -19,27 +20,15 @@ def create_app():
     else:
         app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') #within .env file
         
-    db.init_app(app)
 
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'auth.login'
-
-    from .models import User
     from .views import main_blueprint
-    from .auth import auth_blueprint
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
 
     # Register blueprint for routes
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(auth_blueprint)
 
     #if __name__ == '__main__':
     with app.app_context():
-        print('Creating tables')
-        db.create_all()  # Create tables (if not created)
+        print('Creating index page')
         #app.run(debug=True)
 
     return app
--- website/views.py
+++ website/views.py
@@ -2,55 +2,87 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask import request
 from flask_login import login_required, current_user
 
+import os
+import requests
+
 # Create a blueprint
 main_blueprint = Blueprint('main', __name__)
 
 
 @main_blueprint.route('/', methods=['GET', 'POST'])
-@login_required
-def todo():
+def index():
+    weather_data = None
+    error_msg = None
+
+    #User has submitted info on the form
     if request.method == 'POST':
-        task = request.form['task']
-        new_task = Task(text=task, status='not-completed', user=current_user)
-        db.session.add(new_task)
-        db.session.commit()
+        city = request.form.get('city')
+        state = request.form.get('state')
+        country = request.form.get('country')
+
+        #Making sure neither city or country is empty
+        if city and country:
+            api_key = os.getenv('SECRET_KEY') #Retrieving key from environment
+
+            #URL to retrieve weather data
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={api_key}&units=metric"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                weather_data = response.json()
+            else:
+                error_msg = "Unable to fetch weather data. Please make sure you have the correct inputs"
+
+        else:
+            error_msg = "City and Country are required."
+
+    return render_template('index.html', weather_data = weather_data, error_msg = error_msg)
+
+
+# @login_required
+# def todo():
+#     if request.method == 'POST':
+#         task = request.form['task']
+#         new_task = Task(text=task, status='not-completed', user=current_user)
+#         db.session.add(new_task)
+#         db.session.commit()
         
-    tasks = Task.query.filter_by(user=current_user).all()
-    return render_template('todo.html', tasks=tasks)
+#     tasks = Task.query.filter_by(user=current_user).all()
+#     return render_template('todo.html', tasks=tasks)
 
 
-@main_blueprint.route('/check/<int:task_id>')
-@login_required
-def check(task_id):
-    task = Task.query.get(task_id)
+# @main_blueprint.route('/check/<int:task_id>')
+# @login_required
+# def check(task_id):
+#     task = Task.query.get(task_id)
     
-    if not task or task.user != current_user:
-        return redirect(url_for('main.todo'))
+#     if not task or task.user != current_user:
+#         return redirect(url_for('main.todo'))
     
-    task.status = 'completed'
-    db.session.commit()
+#     task.status = 'completed'
+#     db.session.commit()
 
-    return redirect(url_for('main.todo'))
-
-
-
-@main_blueprint.route('/conditional')
-def conditional():
-    user = 'admin'
-    return render_template('conditional.html', user=user)
+#     return redirect(url_for('main.todo'))
 
 
-@main_blueprint.route('/loop')
-def loop():
-    users = ['admin', 'user', 'guest']
-    return render_template('loop.html', items=users)
+
+# @main_blueprint.route('/conditional')
+# def conditional():
+#     user = 'admin'
+#     return render_template('conditional.html', user=user)
 
 
-@main_blueprint.route('/form', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return f'Logged in as {username}'
+# @main_blueprint.route('/loop')
+# def loop():
+#     users = ['admin', 'user', 'guest']
+#     return render_template('loop.html', items=users)
+
+
+# @main_blueprint.route('/form', methods=['GET', 'POST'])
+# def form():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         return f'Logged in as {username}'
     
-    return render_template('form.html')
+#     return render_template('form.html')
