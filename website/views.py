a718931aa3d7723d9eb55f55f2e82b06a3eb6fd3
Samuel Atilano <samuel4atilano@icloud.com>
2026-02-18 23:14:58 -0500

    Cleaning for Pylint part 4

--- website/views.py
+++ website/views.py
@@ -6,6 +6,7 @@ import os
 from flask import Blueprint, jsonify, render_template
 from flask import request
 import requests
+from website.views import main_blueprint
 
 # Create a blueprint
 main_blueprint = Blueprint('main', __name__)
@@ -13,14 +14,15 @@ main_blueprint = Blueprint('main', __name__)
 #API Key
 weather_api_key = os.getenv('WEATHER_KEY')
 
-"""Route for the index page with form"""
+
 @main_blueprint.route('/')
 def index():
+    """Route for the index page with form"""
     #Render Form
     return render_template("dashboard.html")
 
-"""Helper function to retrieve info from OpenWeather API"""
 def get_weather():
+    """Helper function to retrieve info from OpenWeather API"""
     data = request.get_json()
 
     city = data.get("city")
@@ -31,7 +33,9 @@ def get_weather():
         return {"error": "Missing some fields"}, 400
 
     try: 
-        api_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={weather_api_key}&units=metric")
+        api_response = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={weather_api_key}&units=metric"
+            )
         api_response.raise_for_status()
         weather_data = api_response.json()
 
@@ -41,8 +45,9 @@ def get_weather():
     return {"success": True, "data": weather_data}, 200
 
 
-"""API Route to return OpenWeather Data"""
+
 @main_blueprint.route('/api/v1/weather', methods=['POST'])
 def update_index():
+    """API Route to return OpenWeather Data"""
     weather_data, status = get_weather()
     return jsonify(weather_data), status
